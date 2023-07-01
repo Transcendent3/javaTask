baa16cf17d87dd268a012c421da458c925e312ba
Nikolai <dolinnikolai@gmail.com>
2023-07-01 22:02:45 +0300

    task1

---
+++ lesson2/task1.java
@@ -0,0 +1,41 @@
+package lesson2;
+
+// Дана строка sql-запроса "select * from students where ". Сформируйте часть WHERE этого запроса, 
+// используя StringBuilder. Данные для фильтрации приведены ниже в виде json строки.
+// Если значение null, то параметр не должен попадать в запрос.
+// Параметры для фильтрации: {"name":"Ivanov", "country":"Russia", "city":"Moscow", "age":"null"}
+public class task1 {
+    public static void main(String[] args) throws Exception {
+        // String[] list = lib.ReadLineFromFile("dataForSelect.txt");
+        String[] list = { "{name:Ivanov, country:Russia, city:Moscow, age:null}" };
+        System.out.println(list[0]);
+        StringBuilder resultSelect = LineInList(list[0]);
+        System.out.println(resultSelect);
+    }
+
+    public static StringBuilder LineInList(String line) {
+        String line1 = line.replace("{", "");
+        String line2 = line1.replace("}", "");
+        System.out.println(line2);
+        StringBuilder result = new StringBuilder("select * from students where ");
+
+        String[] arrayData = line2.split(", ");
+        for (int i = 0; i < arrayData.length; i++) {
+            String[] arrData = arrayData[i].split(":");
+            if (arrData[1].equals("null") == false) {
+                if (i != 0) {
+                    result.append(", ");
+                    result.append(arrData[0]);
+                    result.append(" = ");
+                    result.append(arrData[1]);
+                } else {
+                    result.append(arrData[0]);
+                    result.append(" = ");
+                    result.append(arrData[1]);
+                }
+            }
+
+        }
+        return result;
+    }
+}
