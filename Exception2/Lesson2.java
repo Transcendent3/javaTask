3d9fc19cf0c0cb23c439c5d802be950615bf9098
Nikolai <dolinnikolai@gmail.com>
2023-09-02 13:59:16 +0300

    exception2 hw3

--- Exception2/Lesson2.java
+++ Exception2/Lesson2.java
@@ -1,8 +1,5 @@
 package Exception2;
 
-package homework;
-
-
 //Дан следующий код, исправьте его там, где требуется (задание 3 https://docs.google.com/document/d/17EaA1lDxzD5YigQ5OAal60fOFKVoCbEJqooB9XfhT7w/edit)
 public class Lesson2 {
     public static void main(String[] args) {
